fa7a7eb1bdc09e7b819cf9d31a24bf68b3707713
Tatarincev Alexey <tatarincev0810@gmail.com>
2025-11-27 03:04:59 +0300

    test5

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-
-    bucket = "tf-state-atata"
+    bucket                      = "tf-state-atata"
+    # key                         = "state/terraform.tfstate"
     key    = "infra/terraform.tfstate"
-    region = "ru-central1"
+    region                      = "us-east-1"
+
+    endpoint                    = "https://storage.yandexcloud.net"
 
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    use_path_style              = true
+    skip_metadata_api_check     = true
+
+    force_path_style            = true
   }
 }
